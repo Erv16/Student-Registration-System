8aca960ad6558e4c59219223f49cc452c3c98822
Erv16 <epalani1@binghamton.edu>
2019-10-22 20:58:00 -0400

    Revert "Values that could be added to the database"
    This reverts commit 61982a52f8d0ba69f222c8fc179d036b54bf96eb.

--- Content/demo2(1).sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE PROCEDURE 
-show_status(sid_in in students.sid%type,
-    status_out out students.status%type) is
-begin 
-    select status into status_out from students where students.sid = sid_in;
-end;
-/
